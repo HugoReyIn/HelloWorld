a756310e14899759c10d0542af6ead5b45a571e7
HugoReyIn <hugo.rey@opendeusto.es>
2024-02-22 09:35:42 +0100

    Subida inicial

---
+++ Assets/character.cs
@@ -0,0 +1,12 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class character 
+{
+
+    public character (string nombre)
+    {
+        Debug.Log("Hola, soy " + this.name);
+    }
+}
---
+++ Assets/differentCharacters.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class differentCharacters : character
+{
+    public enemy()
+    {
+
+    }
+}
